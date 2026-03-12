5132f9412603900d18e8244d8e4efa10c9644a32
Kevin Ellen <45627930+Kevin-Ellen@users.noreply.github.com>
2026-03-12 21:57:18 +0000

    fix(terraform): add release metadata inputs to infra module

--- infra/variables.tf
+++ infra/variables.tf
@@ -18,14 +18,27 @@ variable "instance_name" {
   type = string
 }
 
-variable "worker_script_path" {
-  type = string
-}
-
 variable "compatibility_date" {
   type = string
 }
 
 variable "subdomain" {
   type = string
+}
+
+variable "worker_script_path" {
+  description = "Absolute or relative path to the Worker entry bundle on disk."
+  type        = string
+}
+
+variable "release_sha" {
+  description = "Git SHA of the release artifact being deployed."
+  type        = string
+  default     = null
+}
+
+variable "release_key" {
+  description = "Artifact storage key for the release bundle being deployed."
+  type        = string
+  default     = null
 }
