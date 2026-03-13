a3c7c5a73d0d17d92ca109a4d9a002bffa73d6db
Kevin Ellen <45627930+Kevin-Ellen@users.noreply.github.com>
2026-03-13 21:36:52 +0000

    (C) Various changes to building & SASS

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -5,10 +5,10 @@ export default {
       "^(?:l|c|m|u|is|has)-[a-z0-9]+(?:-[a-z0-9]+)*(?:__(?:[a-z0-9]+(?:-[a-z0-9]+)*))?(?:--(?:[a-z0-9]+(?:-[a-z0-9]+)*))?$",
       {
         message:
-          "Expected class names to use project prefixes (l-, c-, m-, u-, is-, has-) with optional BEM __element and --modifier."
-      }
+          "Expected class names to use project prefixes (l-, c-, m-, u-, is-, has-) with optional BEM __element and --modifier.",
+      },
     ],
     "no-descending-specificity": null,
-    "scss/at-import-no-partial-leading-underscore": null
-  }
+    "scss/at-import-no-partial-leading-underscore": null,
+  },
 };
