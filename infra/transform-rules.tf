99d1edaf221d616a029dd24b18c804b3c9f16069
Kevin Ellen <45627930+Kevin-Ellen@users.noreply.github.com>
2026-04-09 17:12:28 +0100

    bug-fix-1

--- infra/transform-rules.tf
+++ infra/transform-rules.tf
@@ -7,7 +7,7 @@ resource "cloudflare_ruleset" "photo_path_rewrite" {
   rules = [
     {
       ref         = "photo_rewrite"
-      description = "Rewrite /photo/<image-id>.<variant> to Cloudflare Images"
+      description = "Rewrite /photo/<image-id>.<variant> to Cloudflare Images delivery path"
       enabled     = true
 
       expression = "starts_with(http.request.uri.path, \"/photo/\")"
@@ -17,7 +17,7 @@ resource "cloudflare_ruleset" "photo_path_rewrite" {
       action_parameters = {
         uri = {
           path = {
-            expression = "concat(\"/cdn-cgi/imagedelivery/${var.images_delivery_hash}/\", regex_replace(http.request.uri.path, \"^/photo/([^\\.]+)\\.([^\\.]+)$\", \"\\1/\\2\"))"
+            expression = "regex_replace(http.request.uri.path, \"^/photo/([^.]+)\\.([^.]+)$\", \"/cdn-cgi/imagedelivery/${var.images_delivery_hash}/${1}/${2}\")"
           }
         }
       }
