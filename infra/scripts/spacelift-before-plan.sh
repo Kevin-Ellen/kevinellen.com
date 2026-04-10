43a0c1e98399e918ba14e4460b029bd3a1ca6195
Kevin Ellen <45627930+Kevin-Ellen@users.noreply.github.com>
2026-04-10 18:27:17 +0100

    TF bug-3

--- infra/scripts/spacelift-before-plan.sh
+++ infra/scripts/spacelift-before-plan.sh
@@ -3,6 +3,12 @@ echo "PWD=$(pwd)"
 echo "Terraform files in working directory:"
 find . -maxdepth 2 -name "*.tf" | sort
 
+echo "PWD=$(pwd)"
+echo "main.tf path:"
+realpath ./main.tf
+echo "variables.tf path:"
+realpath ./variables.tf
+
 echo
 echo "deployments/dev/variables.tf contents:"
 cat deployments/dev/variables.tf || true
@@ -14,3 +20,6 @@ cat variables.tf || true
 echo
 echo "searching for photos_namespace_id in workspace:"
 grep -R "photos_namespace_id" . || true
+
+
+
